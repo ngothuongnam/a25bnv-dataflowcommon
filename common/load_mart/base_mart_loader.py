9dd588e076155f206cb5fa38e2af5da1e14ac4e3
namngo <namnt3@synodus.com>
2025-09-05 22:56:57 +0700

    commit

--- common/load_mart/base_mart_loader.py
+++ common/load_mart/base_mart_loader.py
@@ -1,40 +1,41 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit
+import tomli
 import subprocess
 import json
 
 class BaseMartLoader:
-    def __init__(self, table, config):
+    def __init__(self, table, config_path):
         self.table = table
+        self.spark = SparkSession.builder.appName("MartLoader").getOrCreate()
+        if not config_path:
+            raise ValueError("config_path is required for BaseMartLoader")
+        with open(config_path, 'rb') as f:
+            config = tomli.load(f)
         self.columns = [col for col in config['mart']['columns'] if col.lower() != 'pk_id']
         self.jdbc_url = config['mart']['jdbc_url']
         self.jdbc_properties = config['mart']['jdbc_properties']
-        self.spark = SparkSession.builder.appName("MartLoader").getOrCreate()
 
     def load_json(self, json_path, update_time=None):
-        try:
-            check_cmd = ["hdfs", "dfs", "-test", "-e", json_path]
-            result = subprocess.run(check_cmd)
-            if result.returncode != 0:
-                print(f"File {json_path} not found on HDFS. Skipping mart load.")
-                return None
-            df = self.spark.read.option("multiLine", True).json(json_path)
-            if update_time:
-                update_time_str = update_time.replace("-", "")
-                df = df.withColumn("update_time", lit(update_time_str))
-            df = df.select(*self.columns)
-            df.write \
-                .format("jdbc") \
-                .option("url", self.jdbc_url) \
-                .option("dbtable", self.table) \
-                .option("user", self.jdbc_properties['user']) \
-                .option("password", self.jdbc_properties['password']) \
-                .option("driver", self.jdbc_properties['driver']) \
-                .mode("append") \
-                .save()
-            return True
-        finally:
-            self.stop()
+        check_cmd = ["hdfs", "dfs", "-test", "-e", json_path]
+        result = subprocess.run(check_cmd)
+        if result.returncode != 0:
+            print(f"File {json_path} not found on HDFS. Skipping mart load.")
+            return
+        df = self.spark.read.option("multiLine", True).json(json_path)
+        if update_time:
+            update_time_str = update_time.replace("-", "")
+            df = df.withColumn("update_time", lit(update_time_str))
+        df = df.select(*self.columns)
+        df.write \
+            .format("jdbc") \
+            .option("url", self.jdbc_url) \
+            .option("dbtable", self.table) \
+            .option("user", self.jdbc_properties['user']) \
+            .option("password", self.jdbc_properties['password']) \
+            .option("driver", self.jdbc_properties['driver']) \
+            .mode("append") \
+            .save()
 
     def stop(self):
         self.spark.stop()
--- tasks/load_mart/ldnn/company_load_mart.py
+++ tasks/load_mart/ldnn/company_load_mart.py
@@ -1,4 +1,3 @@
-import tomli
 import argparse
 from common.load_mart.ldnn.company_mart_loader import CompanyMartLoader
 from datetime import datetime
@@ -10,11 +9,7 @@ if __name__ == "__main__":
     parser.add_argument('--update-time', required=False)
     args = parser.parse_args()
 
-    with open(args.config, "rb") as f:
-        config = tomli.load(f)
-
     update_time = args.update_time or datetime.now().strftime('%Y-%m-%d')
-    loader = CompanyMartLoader(config)
-    result = loader.load_json(args.json_path, update_time)
-    if result is None:
-        print("Mart load skipped: file not found.")
+    loader = CompanyMartLoader(args.config)
+    loader.load_json(args.json_path, update_time)
+    loader.stop()
