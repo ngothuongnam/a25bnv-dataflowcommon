abc57cad19ddeb33843e8cdcf4f3a229b947f3e5
namngo <namnt3@synodus.com>
2025-09-05 15:57:50 +0700

    commit

--- common/load_mart/base_mart_loader.py
+++ common/load_mart/base_mart_loader.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
 import subprocess
 import json
 
@@ -10,24 +11,30 @@ class BaseMartLoader:
         self.jdbc_properties = config['mart']['jdbc_properties']
         self.spark = SparkSession.builder.appName("MartLoader").getOrCreate()
 
-    def load(self, data_path: str):
-        # Kiểm tra file json trên HDFS
-        check_cmd = ["hdfs", "dfs", "-test", "-e", data_path]
-        result = subprocess.run(check_cmd)
-        if result.returncode != 0:
-            print(f"File {data_path} not found on HDFS. Skipping mart load.")
-            return
-        df = self.spark.read.option("multiLine", True).json(data_path)
-        df = df.select(*self.columns)
-        df.write \
-            .format("jdbc") \
-            .option("url", self.jdbc_url) \
-            .option("dbtable", self.table) \
-            .option("user", self.jdbc_properties['user']) \
-            .option("password", self.jdbc_properties['password']) \
-            .option("driver", self.jdbc_properties['driver']) \
-            .mode("append") \
-            .save()
+    def load_json(self, json_path, update_time=None):
+        try:
+            check_cmd = ["hdfs", "dfs", "-test", "-e", json_path]
+            result = subprocess.run(check_cmd)
+            if result.returncode != 0:
+                print(f"File {json_path} not found on HDFS. Skipping mart load.")
+                return None
+            df = self.spark.read.option("multiLine", True).json(json_path)
+            if update_time:
+                update_time_str = update_time.replace("-", "")
+                df = df.withColumn("update_time", lit(update_time_str))
+            df = df.select(*self.columns)
+            df.write \
+                .format("jdbc") \
+                .option("url", self.jdbc_url) \
+                .option("dbtable", self.table) \
+                .option("user", self.jdbc_properties['user']) \
+                .option("password", self.jdbc_properties['password']) \
+                .option("driver", self.jdbc_properties['driver']) \
+                .mode("append") \
+                .save()
+            return True
+        finally:
+            self.stop()
 
     def stop(self):
         self.spark.stop()
--- tasks/load_mart/ldnn/company_load_mart.py
+++ tasks/load_mart/ldnn/company_load_mart.py
@@ -2,7 +2,6 @@ import tomli
 import argparse
 from common.load_mart.ldnn.company_mart_loader import CompanyMartLoader
 from datetime import datetime
-import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -14,15 +13,8 @@ if __name__ == "__main__":
     with open(args.config, "rb") as f:
         config = tomli.load(f)
 
-    # Cơ chế lấy update_time giống load_staging
-    update_time = args.update_time
-    if not update_time:
-        update_time = datetime.now().strftime('%Y-%m-%d')
-    dt = datetime.strptime(update_time, "%Y-%m-%d")
-    json_path = args.json_path
-    if not os.path.exists(json_path):
-        print(f"File {json_path} not found.")
-        exit(1)
-
+    update_time = args.update_time or datetime.now().strftime('%Y-%m-%d')
     loader = CompanyMartLoader(config)
-    loader.load(json_path)
+    result = loader.load_json(args.json_path, update_time)
+    if result is None:
+        print("Mart load skipped: file not found.")
